c1a23f0f0f061b3c67c5fde67aea35bbf07c74c3
hide-on-bush-x <hideonbushx123@gmail.com>
2024-05-29 21:15:27 -0300

    feat: oracle request handler

---
+++ masa/miner/oracle_request.py
@@ -0,0 +1,51 @@
+import requests
+from ..types import TwitterObject
+class OracleRequest():
+    def __init__(self):
+        self.base_url = "http://localhost:8080/api/v1"
+        self.authorization = "Bearer 1234"
+        self.headers = {"Authorization": self.authorization }
+        
+    def get(self, path) -> requests.Response:
+        return requests.get(f"{self.base_url}{path}", headers=self.headers)
+    
+    def get_profile(self, profile) -> TwitterObject:
+        print(f"Getting profile from oracle {profile}")
+        response = self.get(f"/data/twitter/profile/{profile}")
+        
+        if response.status_code == 504:
+            print("Oracle request failed")
+            return None
+        twitter_profile = self.format_profile(response)
+        
+        return twitter_profile
+        
+    def format_profile(self, data: requests.Response) -> TwitterObject:
+        print(f"Formatting oracle data: {data}")
+        profile_data = data.json()['data']
+        twitter_profile = TwitterObject(
+                    UserID=profile_data.get("UserID", None),
+                    Avatar=profile_data.get("Avatar", None),
+                    Banner=profile_data.get("Banner", None),
+                    Biography=profile_data.get("Biography", None),
+                    Birthday=profile_data.get("Birthday", None),
+                    FollowersCount=profile_data.get("FollowersCount", None),
+                    FollowingCount=profile_data.get("FollowingCount", None),
+                    FriendsCount=profile_data.get("FriendsCount", None),
+                    IsPrivate=profile_data.get("IsPrivate", None),
+                    IsVerified=profile_data.get("IsVerified", None),
+                    Joined=profile_data.get("Joined", None),
+                    LikesCount=profile_data.get("LikesCount", None),
+                    ListedCount=profile_data.get("ListedCount", None),
+                    Location=profile_data.get("Location", None),
+                    Name=profile_data.get("Name", None),
+                    PinnedTweetIDs=profile_data.get("PinnedTweetIDs", None),
+                    TweetsCount=profile_data.get("TweetsCount", None),
+                    URL=profile_data.get("URL", None),
+                    Username=profile_data.get("Username", None),
+                    Website=profile_data.get("Website", None)
+                )
+        
+        return twitter_profile
+            
+
