fc4e4f1e86a690bf9e5c1b5c79b206a89a5e82c0
hide-on-bush-x <hideonbushx123@gmail.com>
2024-05-29 21:14:20 -0300

    feat: adding twitter profile type

---
+++ masa/types.py
@@ -0,0 +1,23 @@
+from typing import TypedDict, Optional, List
+
+class TwitterObject(TypedDict, total=False):
+    UserID: str
+    Avatar: Optional[str]
+    Banner: Optional[str]
+    Biography: Optional[str]
+    Birthday: Optional[str]
+    FollowersCount: Optional[int]
+    FollowingCount: Optional[int]
+    FriendsCount: Optional[int]
+    IsPrivate: Optional[bool]
+    IsVerified: Optional[bool]
+    Joined: Optional[str]
+    LikesCount: Optional[int]
+    ListedCount: Optional[int]
+    Location: Optional[str]
+    Name: Optional[str]
+    PinnedTweetIDs: Optional[List[str]]
+    TweetsCount: Optional[int]
+    URL: Optional[str]
+    Username: Optional[str]
+    Website: Optional[str]
