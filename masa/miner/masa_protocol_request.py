d259c80601eac7034c769ba30e533c6574ad4641
Grant Foster <grantdfoster@gmail.com>
2024-06-05 11:13:39 -0700

    fix: point to deployed orcale

--- masa/miner/masa_protocol_request.py
+++ masa/miner/masa_protocol_request.py
@@ -3,8 +3,8 @@ import requests
 
 class MasaProtocolRequest():
     def __init__(self):
-        self.base_url = os.getenv('ORACLE_BASE_URL', "http://localhost:8080/api/v1")
-        self.authorization = os.getenv('ORACLE_AUTHORIZATION', "Bearer 1234")
+        self.base_url = os.getenv('ORACLE_BASE_URL', "http://54.160.27.4:8080/api/v1")
+        self.authorization = os.getenv('ORACLE_AUTHORIZATION', "")
         self.headers = {"Authorization": self.authorization }
         
     def get(self, path) -> requests.Response:
