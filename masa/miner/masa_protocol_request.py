110f8ff509246a1d1d03848bb42de7783b1122df
Grant Foster <grantdfoster@gmail.com>
2024-06-05 09:27:15 -0700

    fix: class and variable renamed

--- masa/miner/masa_protocol_request.py
+++ masa/miner/masa_protocol_request.py
@@ -1,16 +1,16 @@
 import os
 import requests
 
-class ProtocolRequest():
+class MasaProtocolRequest():
     def __init__(self):
         self.base_url = os.getenv('ORACLE_BASE_URL', "http://localhost:8080/api/v1")
         self.authorization = os.getenv('ORACLE_AUTHORIZATION', "Bearer 1234")
         self.headers = {"Authorization": self.authorization }
         
     def get(self, path) -> requests.Response:
-        timeout_duration = 1
-        return requests.get(f"{self.base_url}{path}", headers=self.headers, timeout=timeout_duration)
+        timeout_in_seconds = 1
+        return requests.get(f"{self.base_url}{path}", headers=self.headers, timeout=timeout_in_seconds)
     
     def post(self, path, body) -> requests.Response:
-        timeout_duration = 1
-        return requests.post(f"{self.base_url}{path}", json=body, headers=self.headers, timeout=timeout_duration)
+        timeout_in_seconds = 1
+        return requests.post(f"{self.base_url}{path}", json=body, headers=self.headers, timeout=timeout_in_seconds)
