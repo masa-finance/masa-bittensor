f613da3550a6070525659c362b6df31f22daefbb
hide-on-bush-x <hideonbushx123@gmail.com>
2024-06-06 12:11:16 -0300

    feat(twitter-parsers): improving naming

--- masa/validator/twitter/followers/parser.py
+++ masa/validator/twitter/followers/parser.py
@@ -1,7 +1,7 @@
 from masa.types.twitter import TwitterFollowerObject
 
-def followers_parser(followers):
+def followers_parser(follower_responses):
     return [
         [TwitterFollowerObject(**follower) for follower in response]
-        for response in followers  # Each response is a list of dictionaries
+        for response in follower_responses  # Each response is a list of dictionaries
     ]
--- masa/validator/twitter/tweets/parser.py
+++ masa/validator/twitter/tweets/parser.py
@@ -1,7 +1,7 @@
 from masa.types.twitter import TwitterTweetObject
 
-def tweets_parser(tweets):
+def tweets_parser(tweet_responses):
     return [
         [TwitterTweetObject(**tweet) for tweet in response]
-        for response in tweets  # Each response is a list of dictionaries
+        for response in tweet_responses  # Each response is a list of dictionaries
     ]
