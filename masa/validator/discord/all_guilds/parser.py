1e2a364c0aafdaa88ef1ef18f87c625add704c71
obasilakis <vasileios@masa.finance>
2024-06-20 18:14:25 +0300

    fix: return list of dicts instead of list of list of dicts

--- masa/validator/discord/all_guilds/parser.py
+++ masa/validator/discord/all_guilds/parser.py
@@ -1,7 +1,8 @@
-from masa.types.discord import DiscordGuildObject
+from masa.types.discord import DiscordGuildChannelObject
 
 def all_guilds_parser(all_guilds_responses):
     return [
-        [DiscordGuildObject(**guild) for guild in response]
+        DiscordGuildChannelObject(**guild)
         for response in all_guilds_responses  # Each response is a list of dictionaries
+        for guild in response
     ]
