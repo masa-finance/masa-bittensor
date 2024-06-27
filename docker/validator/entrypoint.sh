d0f90bfe8e9991eeabe5049d6606627e622de1d4
JD <156010594+5u6r054@users.noreply.github.com>
2024-06-27 11:35:13 -0700

    update validator start

--- docker/validator/entrypoint.sh
+++ docker/validator/entrypoint.sh
@@ -34,8 +34,8 @@ $COLDKEY_PASSWORD
 y
 EOF
 
-echo "Wait 2250s before boost"
-sleep 2250
+echo "Wait 1800 before boost"
+sleep 1800
 
 echo "# Boost subnet on the root subnet"
 echo "1" | btcli root boost --netuid 1 --increase 1 --wallet.name validator --wallet.hotkey validator_hotkey --subtensor.chain_endpoint ws://subtensor_machine:9945 <<EOF
@@ -43,11 +43,20 @@ $COLDKEY_PASSWORD
 y
 EOF
 
+# Define a function to start the validator
+start_validator() {
+    python /app/neurons/validator.py --netuid 1 --subtensor.chain_endpoint ws://subtensor_machine:9946 --wallet.name validator --wallet.hotkey validator_hotkey --axon.port 8092
+}
+
 # Attempt to register the validator and start it
 if register_node validator; then
     echo "Validator registration successful. Starting the validator..."
+
     # Start the validator
-    python /app/neurons/validator.py --netuid 1 --subtensor.chain_endpoint ws://subtensor_machine:9946 --wallet.name validator --wallet.hotkey validator_hotkey --axon.port 8092
+    start_validator &
+    VALIDATOR_PID=$!
+    echo "Validator started with PID $VALIDATOR_PID"
+
 else
     echo "Validator registration failed. Not starting the validator."
 fi
