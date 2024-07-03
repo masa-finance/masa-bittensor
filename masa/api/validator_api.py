9dd137dd7beaaca91f08104e67e7707bb5c4b355
obasilakis <vasileios.georgopoulos@gmail.com>
2024-07-03 20:44:25 +0300

    fix: don't flatten user guilds before setting scores

--- masa/api/validator_api.py
+++ masa/api/validator_api.py
@@ -154,7 +154,8 @@ class ValidatorAPI:
         return all_responses[0]
 
     async def get_discord_user_guilds(self):
-        return await DiscordUserGuildsForwarder(self.validator).forward_query()
+        all_responses = await DiscordUserGuildsForwarder(self.validator).forward_query()
+        return all_responses[0]
 
     async def get_discord_all_guilds(self):
         return await DiscordAllGuildsForwarder(self.validator).forward_query()
