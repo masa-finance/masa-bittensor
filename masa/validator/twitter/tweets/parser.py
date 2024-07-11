a9b56462d3507facce578b60177d6083ff8b1fcc
Grant Foster <grant@masa.finance>
2024-07-11 13:11:55 -0700

    gdf/validator-cleanup-and-doc-update (#151)
    * fix: cleans up console, updates docs, organizes web scraper
    
    * fix(validator): fixes response handling
    
    * fix: longer timeout on validator

--- masa/validator/twitter/tweets/parser.py
+++ masa/validator/twitter/tweets/parser.py
@@ -3,6 +3,6 @@ from masa.types.twitter import TwitterTweetObject
 
 def tweets_parser(tweet_responses):
     return [
-        [TwitterTweetObject(**tweet) for tweet in response]
+        [TwitterTweetObject(**tweet) for tweet in response if isinstance(tweet, dict)]
         for response in tweet_responses  # Each response is a list of dictionaries
     ]
