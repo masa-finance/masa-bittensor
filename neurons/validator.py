7966d1cd954485a5fc659b9be4809126283381b9
Grant Foster <grantdfoster@gmail.com>
2024-08-30 17:57:21 -0700

    fix: adjust pulse check count

--- neurons/validator.py
+++ neurons/validator.py
@@ -84,7 +84,7 @@ class Validator(BaseValidatorNeuron):
 
         bt.logging.info(f"Fetching {query} from miner")
         responses = await TwitterTweetsForwarder(self).forward_query(
-            tweet_query=RecentTweetsQuery(query=query, count=1), limit=limit
+            tweet_query=RecentTweetsQuery(query=query, count=3), limit=limit
         )
         all_responses.extend(responses)
 
