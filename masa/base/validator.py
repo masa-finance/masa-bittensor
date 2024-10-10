4b94fc1f81ba472151c43fa3f6170b4338c7d6b6
Grant Foster <grantdfoster@gmail.com>
2024-10-10 10:35:58 -0700

    fix: forwarder and scorer init sooner

--- masa/base/validator.py
+++ masa/base/validator.py
@@ -30,6 +30,9 @@ from masa.base.neuron import BaseNeuron
 from masa.utils.config import add_validator_args
 from masa.mock import MockDendrite
 
+from masa.validator.scorer import Scorer
+from masa.validator.forwarder import Forwarder
+
 
 class BaseValidatorNeuron(BaseNeuron):
     """
@@ -45,6 +48,9 @@ class BaseValidatorNeuron(BaseNeuron):
 
     def __init__(self, config=None):
         super().__init__(config=config)
+
+        self.forwarder = Forwarder(self)
+        self.scorer = Scorer(self)
 
         self.hotkeys = copy.deepcopy(self.metagraph.hotkeys)
         self.tempo = self.subtensor.get_subnet_hyperparameters(self.config.netuid).tempo
