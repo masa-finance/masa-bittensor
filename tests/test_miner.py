38c23da0cf1d14da9d98814c3fe74443448a88e3
Grant Foster <grantdfoster@gmail.com>
2024-11-06 09:19:33 -0800

    fix: remove miner test for protocol

--- tests/test_miner.py
+++ tests/test_miner.py
@@ -52,19 +52,22 @@ class TestMiner:
         uid = miner_instance.uid
         assert uid > -1, "UID should be greater than -1 for success"
 
-    def test_miner_protocol_profile_request(self):
-        synapse = TwitterProfileSynapse(username="getmasafi")
-        profile = TwitterProfileRequest().get_profile(synapse=synapse)
-        assert profile is not None, "profile should not be None"
+    # TODO CI/CD yet to support the protocol node
+    # def test_miner_protocol_profile_request(self):
+    #     synapse = TwitterProfileSynapse(username="getmasafi")
+    #     profile = TwitterProfileRequest().get_profile(synapse=synapse)
+    #     assert profile is not None, "profile should not be None"
 
-    def test_miner_protocol_followers_request(self):
-        synapse = TwitterFollowersSynapse(username="getmasafi", count=3)
-        followers = TwitterFollowersRequest().get_followers(synapse=synapse)
-        assert followers is not None, "followers should not be None"
-        assert len(followers) > 0, "followers should exist"
+    # TODO CI/CD yet to support the protocol node
+    # def test_miner_protocol_followers_request(self):
+    #     synapse = TwitterFollowersSynapse(username="getmasafi", count=3)
+    #     followers = TwitterFollowersRequest().get_followers(synapse=synapse)
+    #     assert followers is not None, "followers should not be None"
+    #     assert len(followers) > 0, "followers should exist"
 
-    def test_miner_protocol_tweets_request(self):
-        synapse = RecentTweetsSynapse(query="btc", count=3)
-        tweets = TwitterTweetsRequest(10).get_recent_tweets(synapse=synapse)
-        assert tweets is not None, "tweets should not be None"
-        assert len(tweets) > 0, "tweets should exist"
+    # TODO CI/CD yet to support the protocol node
+    # def test_miner_protocol_tweets_request(self):
+    #     synapse = RecentTweetsSynapse(query="btc", count=3)
+    #     tweets = TwitterTweetsRequest(10).get_recent_tweets(synapse=synapse)
+    #     assert tweets is not None, "tweets should not be None"
+    #     assert len(tweets) > 0, "tweets should exist"
