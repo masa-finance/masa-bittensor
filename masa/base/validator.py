d6cea929b9b2c749fe8ac292895f9d604d13ece0
5u6r054 <john@masa.finance>
2025-02-14 19:13:32 -0800

    attractive logs

--- masa/base/validator.py
+++ masa/base/validator.py
@@ -329,7 +329,6 @@ class BaseValidatorNeuron(BaseNeuron):
 
     async def resync_metagraph(self):
         """Resyncs the metagraph and updates the hotkeys and moving averages based on the new metagraph."""
-        bt.logging.info("resync_metagraph()")
 
         # Copies state of metagraph before syncing.
         previous_metagraph = copy.deepcopy(self.metagraph)
